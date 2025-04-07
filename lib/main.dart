f1b9d3470361109cef8e5d9b9c6503febb32bf2a
Aluno Feira <aluno.fsa@senaiba.com.br>
2025-04-07 10:42:05 -0300

    teste

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Site de Jurubeba',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 214, 2, 2)),
+      ),
+      home: const MyHomePage(title: '                                                             Contador profissional'),
+      debugShowCheckedModeBanner: false,
+    );
+
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+    
+      _counter = _counter + 10;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   
+    
+    return Scaffold(
+      appBar: AppBar(
+       
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        
+        title: Text(widget.title),
+      ),
+      body: Center(
+        
+        child: Column(
+         
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Contador:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.donut_small_rounded),
+      ), 
+    );
+  }
+}
